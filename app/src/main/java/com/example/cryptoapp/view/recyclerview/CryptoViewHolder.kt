8730ae68f14e6469f5138284d8d9f0144b076ef2
PIXXXA <itchy.scratchy159@gmail.com>
2021-08-24 23:18:28 +0300

    update string builder method

--- app/src/main/java/com/example/cryptoapp/view/recyclerview/CryptoViewHolder.kt
+++ app/src/main/java/com/example/cryptoapp/view/recyclerview/CryptoViewHolder.kt
@@ -10,19 +10,15 @@ class CryptoViewHolder(private val binding: RecyclerViewItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun populate(modelData: CryptoDataListModel) {
-        binding.cryptoTitle.text = setViewFieldText(R.string.recycler_title, modelData.name)
+        val resources = binding.root.context.resources
+        binding.cryptoTitle.text =
+            "${resources.getString(R.string.recycler_title)} ${modelData.name}"
         binding.growthIndicator.comparePercentChanges(modelData.percent_change_1h)
-        binding.cryptoPrice.text = setViewFieldText(R.string.recycler_price, modelData.price_usd)
-        binding.cryptoVolume.text = setViewFieldText(R.string.recycler_volume, modelData.volume24)
+        binding.cryptoPrice.text =
+            "${resources.getString(R.string.recycler_price)} ${modelData.price_usd}"
+        binding.cryptoVolume.text =
+            "${resources.getString(R.string.recycler_volume)} ${modelData.volume24}"
         binding.cryptoPercentChange24.text =
-            setViewFieldText(R.string.recycler_percent_change_24, modelData.percent_change_24h)
-    }
-
-    private fun setViewFieldText(stringResources: Int, modelField: Any): String {
-        return binding.root.context.getString(stringResources).plus(EMPTY_STRING).plus(modelField)
-    }
-
-    companion object {
-        const val EMPTY_STRING = " "
+            "${resources.getString(R.string.recycler_percent_change_24)} ${modelData.percent_change_24h}"
     }
 }
